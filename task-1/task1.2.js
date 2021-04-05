e7e4a5cbbbaf94a841b7bd7451621c5ea67b4d0a
Diana Loika <diana_loika@epam.com>
2021-04-06 00:37:00 +0300

    fix: use pipeline, transform, highWaterMark

--- task-1/task1.2.js
+++ task-1/task1.2.js
@@ -1,21 +1,44 @@
 import csv from 'csvtojson';
 import fs from 'fs';
+import { pipeline, Transform } from 'stream';
 
 const csvFilePath= './task-1/csv/nodejs-hw1-ex1.csv';
 const outputFilePath = './task-1/csv/csv-to-txt.txt';
 
 const writeStream = fs.createWriteStream(outputFilePath, 'utf8');
+const readStream = fs.createReadStream(csvFilePath, {highWaterMark: 20});
 
 writeStream.on('error', (err) => {
     console.log('writeStream error', err);
 });
 
-csv({ checkType: true })
-    .fromFile(csvFilePath)
-    .on('data', (line) => {
-        writeStream.write(line)
-
-    })
-    .on('error', (err) => {
-        console.log('error while parsing data', err);
+const stringTransform = new Transform();
+stringTransform._transform = function(chunk, encoding, done) {
+    const stringToObj = JSON.parse(chunk.toString());
+    const newObj = {};
+    Object.entries(stringToObj).forEach(([key, value]) => {
+        if (key === 'Amount') {
+            return;
+        }
+        if (key === 'Price') {
+            newObj[key.toLowerCase()] = Number(value);
+            return;
+        }
+        newObj[key.toLowerCase()] = value;
     });
+    done(null, JSON.stringify(newObj) + '\n');
+};
+
+pipeline(
+    readStream,
+    csv(),
+    stringTransform,
+    writeStream,
+    (err) => {
+        if (err) {
+          console.error('Pipeline failed.', err);
+        } else {
+          console.log('Pipeline succeeded.');
+        }
+    }
+);
