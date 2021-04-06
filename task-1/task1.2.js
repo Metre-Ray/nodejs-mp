5882292d66e9c77dda06071ace889e672a056e75
Diana Loika <diana_loika@epam.com>
2021-04-07 01:57:31 +0300

    Task 2 Method 2 with readline module

--- task-1/task1.2.js
+++ task-1/task1.2.js
@@ -1,16 +1,11 @@
 import csv from 'csvtojson';
 import fs from 'fs';
 import { pipeline, Transform } from 'stream';
+import { createInterface } from 'readline';
+
 
 const csvFilePath= './task-1/csv/nodejs-hw1-ex1.csv';
 const outputFilePath = './task-1/csv/csv-to-txt.txt';
-
-const writeStream = fs.createWriteStream(outputFilePath, 'utf8');
-const readStream = fs.createReadStream(csvFilePath, {highWaterMark: 20});
-
-writeStream.on('error', (err) => {
-    console.log('writeStream error', err);
-});
 
 const stringTransform = new Transform();
 stringTransform._transform = function(chunk, encoding, done) {
@@ -29,16 +24,42 @@ stringTransform._transform = function(chunk, encoding, done) {
     done(null, JSON.stringify(newObj) + '\n');
 };
 
-pipeline(
-    readStream,
-    csv(),
-    stringTransform,
-    writeStream,
-    (err) => {
-        if (err) {
-          console.error('Pipeline failed.', err);
-        } else {
-          console.log('Pipeline succeeded.');
-        }
-    }
-);
+
+// Method 1
+
+// const writeStream = fs.createWriteStream(outputFilePath, 'utf8');
+// const readStream = fs.createReadStream(csvFilePath, {highWaterMark: 20});
+
+// pipeline(
+//     readStream,
+//     csv(),
+//     stringTransform,
+//     writeStream,
+//     (err) => {
+//         if (err) {
+//           console.error('Pipeline failed.', err);
+//         } else {
+//           console.log('Pipeline succeeded.');
+//         }
+//     }
+// );
+
+
+// Method 2
+
+const writeStream = fs.createWriteStream(outputFilePath, 'utf8');
+const readStream = fs.createReadStream(csvFilePath);
+const readlineInterface = createInterface({ input: readStream });
+const converter = csv();
+converter.pipe(stringTransform).pipe(writeStream);
+
+readlineInterface.on('line', (line) => {
+    return converter.write(line + '\n');
+});
+
+writeStream.on('error', (err) => {
+    console.log('writeStream error', err);
+});
+readStream.on('error', (err) => {
+    console.log('readStream error', err);
+});
