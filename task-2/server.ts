04cadca05dcfef361b49518b617aa023c8c381f0
Diana Loika <diana_loika@epam.com>
2021-05-11 03:22:38 +0300

    Implement Task 5

--- task-2/server.ts
+++ task-2/server.ts
@@ -4,6 +4,8 @@ import process from 'process';
 import { userRouter } from './routers/user-route';
 import { initDBData } from './database/dbDataInit';
 import { groupRouter } from './routers/group-route';
+import morgan from 'morgan';
+import { logger } from './loggers/loggers';
 
 const PORT = process.env.PORT || 3456;
 
@@ -11,10 +13,27 @@ const app = express();
 
 app.set('x-powered-by', false);
 app.use(express.json());
+app.use(morgan('tiny', { stream: { write: message => logger.info(message) } }));
 app.use('/users', userRouter);
 app.use('/groups', groupRouter);
+
+// Error handler 1: Express Error middleware
+app.use((err, req, res, next) => {
+    logger.error(err, { exceptionType: 'Error from middleware' });
+    res.status(500).send({ error: `${err.name} ${err.message}` });
+});
 
 app.listen(PORT, async () => {
     await initDBData();
     return process.stdout.write(`server is listening on ${PORT}`);
+});
+
+// Error handler 2: Node's uncaughtException handler for unhandled error inside callback of some function called from route handler
+process.on('uncaughtException', (err: any) => {
+    logger.error(err, { exceptionType: 'Uncaught Exception' });
+});
+
+// Error handler 3: handler for unhandled promise rejection
+process.on('unhandledRejection', (err: any) => {
+    logger.error(err, { exceptionType: 'Unhandled Rejection' });
 });
